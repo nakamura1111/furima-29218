613cf888333c8cd74a97676efda961e5ab3dec52
nakamura1111 <5040.armuakan@gmail.com>
2020-08-28 10:54:25 +0900

    Userモデルのマイグレーション

--- db/migrate/20200828013642_devise_create_users.rb
+++ db/migrate/20200828013642_devise_create_users.rb
@@ -7,6 +7,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :nickname,          null: false
+      t.string :given_name,        null: false
+      t.string :family_name,       null: false
+      t.string :given_name_kana,   null: false
+      t.string :family_name_kana,  null: false
+      t.string :birth_day,         null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
