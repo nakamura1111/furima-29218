88a53bf03b6dac43d6d420620bf688a3d0df386b
nakamura1111 <5040.armuakan@gmail.com>
2020-08-28 20:46:12 +0900

    バリデーション表記の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   validates :nickname,         presence: true
   validates :email,            presence: true, uniqueness: true
-  validates :password,         presence: true, length: {minimum: 6}
+  validates :password,         presence: true, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/ }
   validates :given_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :family_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/ }
   validates :given_name_kana,  presence: true, format: {with: /\A[ァ-ン]+\z/ }
