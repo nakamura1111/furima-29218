c3187be2cad577ec0d6e9c1448f23998360c454b
nakamura1111 <5040.armuakan@gmail.com>
2020-08-29 16:14:06 +0900

    Goodモデルの作成&DBマイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_28_013642) do
+ActiveRecord::Schema.define(version: 2020_08_29_064548) do
+
+  create_table "goods", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "price", null: false
+    t.integer "delivery_fee", null: false
+    t.integer "origin_area_id", null: false
+    t.integer "delivery_days_id", null: false
+    t.integer "fee_charger_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_goods_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2020_08_28_013642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "goods", "users"
 end
