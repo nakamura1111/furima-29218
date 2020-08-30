38cec0cf9264181b1a5e1f260ec0bdb4ec3fd731
nakamura1111 <5040.armuakan@gmail.com>
2020-08-30 18:49:40 +0900

    未ログイン時のログイン画面への遷移

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -1,4 +1,6 @@
 class GoodsController < ApplicationController
+  before_action :move_to_login, except: [:index]
+
   def index
     
   end
@@ -9,6 +11,13 @@ class GoodsController < ApplicationController
 
   def create
 
+  end
+
+  private
+  def move_to_login
+    unless user_signed_in?
+      redirect_to(new_user_session_url)
+    end
   end
   
 end
