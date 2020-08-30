a706260804f3a50e4b98962f60065c39fd0e0e25
nakamura1111 <5040.armuakan@gmail.com>
2020-08-30 19:08:51 +0900

    imageカラムの作成＆DBの入力ミス修正

--- app/models/good.rb
+++ app/models/good.rb
@@ -1,28 +1,24 @@
 class Good < ApplicationRecord
+  # バリデーション
   validates :name,                 presence: true
   validates :description,          presence: true
-  validates :status_id,            presence: true
+  validates :category_id,          presence: true, numericality: { other_than: 1 }
+  validates :status_id,            presence: true, numericality: { other_than: 1 }
   validates :price,                presence: true, numericality: {greater_than_or_equal_to: 300, less_than: 10000000}
   validates :delivery_fee,         presence: true
-  validates :origin_prefecture_id, presence: true
-  validates :delivery_days_id,     presence: true
-  validates :fee_charger_id,       presence: true
+  validates :origin_prefecture_id, presence: true, numericality: { other_than: 1 }
+  validates :delivery_days_id,     presence: true, numericality: { other_than: 1 }
+  validates :fee_charger_id,       presence: true, numericality: { other_than: 1 }
 
+  # アソシエーション
   belongs_to :user
 
+  # ActiveHashのアソシエーション
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-  validates :category_id, presence: true, numericality: { other_than: 1 }
-
   belongs_to_active_hash :good_status
-  validates :status_id, presence: true, numericality: { other_than: 1 }
-
   belongs_to_active_hash :fee_charger
-  validates :fee_charger_id, presence: true, numericality: { other_than: 1 }
-
   belongs_to_active_hash :prefecture
-  validates :origin_area_id, presence: true, numericality: { other_than: 1 }
-
   belongs_to_active_hash :delivery_day
-  validates :delivery_days_id, presence: true, numericality: { other_than: 1 }
+  
 end
--- db/migrate/20200829064548_create_goods.rb
+++ db/migrate/20200829064548_create_goods.rb
@@ -1,16 +1,16 @@
 class CreateGoods < ActiveRecord::Migration[6.0]
   def change
     create_table :goods do |t|
-      t.string       :name,             null: false
-      t.text         :description,      null: false
-      t.integer      :category_id,      null: false
-      t.integer      :status_id,        null: false
-      t.integer      :price,            null: false
-      t.integer      :delivery_fee,     null: false
-      t.integer      :origin_area_id,   null: false
-      t.integer      :delivery_days_id, null: false
-      t.integer      :fee_charger_id,   null: false
-      t.references   :user,             null: false, foreign_key: true
+      t.string       :name,                 null: false
+      t.text         :description,          null: false
+      t.integer      :category_id,          null: false
+      t.integer      :status_id,            null: false
+      t.integer      :price,                null: false
+      t.integer      :delivery_fee,         null: false
+      t.integer      :origin_prefecture_id, null: false
+      t.integer      :delivery_days_id,     null: false
+      t.integer      :fee_charger_id,       null: false
+      t.references   :user,                 null: false, foreign_key: true
 
       t.timestamps
     end
