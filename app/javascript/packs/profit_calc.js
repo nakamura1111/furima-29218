160ceb0773cbf3304d664112cb2301c5737a17e7
nakamura1111 <5040.armuakan@gmail.com>
2020-08-31 20:19:52 +0900

    再入力時の手数料・利益の表示のための修正

--- app/javascript/packs/profit_calc.js
+++ app/javascript/packs/profit_calc.js
@@ -1,27 +1,31 @@
-function profit_calc(){
-  const input_price = document.getElementById("item-price");
+function input_price(){
+  const price = document.getElementById("item-price");
   // 価格の入力フォームの値を検知が一回になるように設定
-  if (input_price.getAttribute("data-input") != null) {
+  if (price.getAttribute("data-input") != null) {
     return null;
   }
-  input_price.setAttribute("data-input", "true");
+  price.setAttribute("data-input", "true");
   // 価格入力フォームに入力があった場合、
-  input_price.addEventListener('input', function(){
-    // リクエスト情報の作成
-    const XHR = new XMLHttpRequest();
-    XHR.open("GET", `/goods/profit_calc/?price=${input_price.value}`, true);
-    XHR.responseType = "json";
-    XHR.send();
-    // レスポンスが帰ってきたら、
-    XHR.onload = () => {
-      const profit = document.getElementById("profit");
-      const item = XHR.response.post;
-      const tax = document.getElementById("add-tax-price");
-      // 利益と販売手数料の描画
-      profit.innerHTML = item.profit;
-      tax.innerHTML = item.sales_fee;
-    }
-  });
+  price.addEventListener('input', profit_calc);
 }
 
-window.setInterval(profit_calc, 1000)
+function profit_calc(){
+  // リクエスト情報の作成
+  const price = document.getElementById("item-price");
+  const XHR = new XMLHttpRequest();
+  XHR.open("GET", `/goods/profit_calc/?price=${price.value}`, true);
+  XHR.responseType = "json";
+  XHR.send();
+  // レスポンスが帰ってきたら、
+  XHR.onload = () => {
+    const profit = document.getElementById("profit");
+    const item = XHR.response.post;
+    const tax = document.getElementById("add-tax-price");
+    // 利益と販売手数料の描画
+    profit.innerHTML = item.profit;
+    tax.innerHTML = item.sales_fee;
+  }
+}
+
+window.addEventListener("load", profit_calc)      // 誤入力時にフォームに値が入っていた場合の初期設定
+window.setInterval(input_price, 1000)
