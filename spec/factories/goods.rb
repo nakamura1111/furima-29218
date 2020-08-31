2f7e706aa324b838fdcc7b5a58705b426bf1d957
nakamura1111 <5040.armuakan@gmail.com>
2020-08-31 18:57:17 +0900

    Fakerの日本語対応、FactoryBotによるGoodモデルのレコード生成

--- spec/factories/goods.rb
+++ spec/factories/goods.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :good do
-    
+    name                 { Faker::Lorem.paragraph_by_chars(number: 40) }
+    description          { Faker::Lorem.paragraph_by_chars(number: 1000) }
+    category_id          { Category.where(id: 2..).sample.id }         
+    status_id            { GoodStatus.where(id: 2..).sample.id }           
+    price                { Faker::Number.between(from: 300, to: 9999999) }
+    origin_prefecture_id { Prefecture.where(id: 2..).sample.id }
+    delivery_days_id     { DeliveryDay.where(id: 2..).sample.id }
+    fee_charger_id       { FeeCharger.where(id: 2..).sample.id }
+    association :user
   end
 end
