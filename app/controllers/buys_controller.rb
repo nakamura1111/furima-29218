7ca0fc01e9cf7772037a05f00f777d8a04f61f07
nakamura1111 <5040.armuakan@gmail.com>
2020-09-02 19:19:07 +0900

    クレジットカード決済と購入履歴のFormオブジェクトの分離

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,14 +1,19 @@
 class BuysController < ApplicationController
   before_action :move_to_login
   def index
+    @buy_info = BuyInfo.new
+    @pay_info = Credit.new
     @good = Good.find(params[:good_id])
   end
 
   def create
     @good = Good.find(params[:good_id])
     @buy_info = BuyInfo.new(buy_params)
-    if @buy_info.valid? && ENV["PAYJP_SECRET_KEY"] != nil                 # 自分で定義したsaveメソッドだと自動バリデーションは行ってくれないので、まずはバリデーションする。
+    @pay_info = Credit.new(credit_params)
+    binding.pry
+    if @buy_info.valid? && @pay_info.valid? && ENV["PAYJP_SECRET_KEY"] != nil                 # 自分で定義したsaveメソッドだと自動バリデーションは行ってくれないので、まずはバリデーションする。
       @buy_info.save
+      @pay_info.save
       redirect_to root_path
     else
       render :index
@@ -25,7 +30,11 @@ class BuysController < ApplicationController
     params.permit(
       :postal_code,:addr_prefecture_id, :addr_municipality,
       :addr_house_number, :addr_building, :tel_number,
-      :good_id, :token
-    ).merge(user_id: current_user.id, price: @good.price)
+      :good_id
+    ).merge(user_id: current_user.id)
+  end
+
+  def credit_params
+    params.permit(:token).merge(price: @good.price)
   end
 end
--- app/forms/buy_info.rb
+++ app/forms/buy_info.rb
@@ -4,20 +4,16 @@ class BuyInfo
   # カラム
   attr_accessor :user_id, :good_id, :buy_history_id,
                 :postal_code,:addr_prefecture_id, :addr_municipality,
-                :addr_house_number, :addr_building, :tel_number,
-                :token, :price
+                :addr_house_number, :addr_building, :tel_number
 
   # バリデーション（BuyerとBuyHistoryモデルとクレジットカード情報）
-  validates :postal_code, presence: true, format: {with: /\A\d{3}[-]\d{4}\z/ }
-  validates :addr_prefecture_id, presence: true, numericality: { other_than: 1 }
-  validates :addr_municipality, presence: true
-  validates :addr_house_number, presence: true
-  validates :tel_number, presence: true
-  validates :token, presence: true
-  validates :price, presence: true, 
-                    numericality: {
-                      only_integer: true, greater_than_or_equal_to: 300, less_than: 10_000_000
-                    }
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A\d{3}[-]\d{4}\z/ }
+    validates :addr_prefecture_id, numericality: { other_than: 1 }
+    validates :addr_municipality
+    validates :addr_house_number
+    validates :tel_number, length: {maximum: 11}, numericality: true 
+  end
   
   # 保存時に使用するメソッドの設定（ActiveRecordのオーバーロード）
   def save
@@ -27,7 +23,5 @@ class BuyInfo
       addr_house_number: addr_house_number, addr_building: addr_building, tel_number: tel_number,
       buy_history_id: buy_history.id
     )
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(amount: price, card: token, currency: 'jpy')
   end
 end
